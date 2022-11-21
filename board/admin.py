1dd1479326b571e8f099058350d8a65ac700addb
Daeyeon <92428865+cooling12345@users.noreply.github.com>
2022-11-21 13:20:25 +0900

    Update admin.py

--- board/admin.py
+++ board/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Board,Reply
+from .models import Board,Reply,User
 # Register your models here.
 admin.site.register(Board)
 admin.site.register(Reply)
+admin.site.register(User)
